ab1f01d93cb43aab99b22d70f273b4f4c04f239d
Gopi Shankar Ravady <gopi2605@gmail.com>
2024-05-24 15:21:17 -0500

    integrated firebase for login page

---
+++ src/components/navbar.tsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+import { auth } from "../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { signOut } from "firebase/auth";
+
+export const Navbar = () => {
+  const [user] = useAuthState(auth);
+
+  const signUserOut = async () => {
+    await signOut(auth);
+  }
+
+  return (<div>
+    <Link to="/"> HOME </Link>
+    <Link to="/login"> LOGIN </Link>
+    <div>
+      {user &&
+        (<>
+          <p>{user?.displayName} - {auth.currentUser?.email}</p>
+          <img src={user?.photoURL || ""} width="100" height="100" />
+          <button onClick={signUserOut}> Log Out </button>
+          </>
+        )}
+    </div>
+  </div>
+  );
+}
